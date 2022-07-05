1ad95de6b442fe6b007c2dbcaab824861924a43b
YASHAS SHETTY K <108472321+yashasshettyk@users.noreply.github.com>
2022-07-05 15:43:37 +0530

    Update revstring.py

--- revstring.py
+++ revstring.py
@@ -1,3 +1,23 @@
+# Given a String S, reverse the string without reversing its individual words. Words are separated by dots.
+
+# Example 1:
+
+# Input:
+# S = i.like.this.program.very.much
+# Output: much.very.program.this.like.i
+# Explanation: After reversing the whole
+# string(not individual words), the input
+# string becomes
+# much.very.program.this.like.i
+# Example 2:
+
+# Input:
+# S = pqr.mno
+# Output: mno.pqr
+# Explanation: After reversing the whole
+# string , the input string becomes
+# mno.pqr
+
 data = input("Enter a stentence without spacing seperated by "." (dots), eg: what.is.your.name")
 
 class Reverser:
