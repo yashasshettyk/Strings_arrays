8710e7d2012bf960b5586631b37e3700133250af
YASHAS SHETTY K <108472321+yashasshettyk@users.noreply.github.com>
2022-07-07 11:14:42 +0530

    Add files via upload

---
+++ yashas.py
@@ -0,0 +1,27 @@
+from typing import final
+
+
+class longestprefixsolution:
+    def prefixsolution(self,array):
+        if len(array)==0:
+            return -1
+        final = ""
+        minlen = len(array[0])
+        for i in range(len(array)):
+            minlen = min(len(array[i]), minlen)
+        for i in range(minlen):
+            char = array[0][i]
+            for j in range(len(array)):
+                if char != array[j][i]:
+                    return -1
+            final+=char
+            
+        return final
+    
+    
+if __name__ == "__main__":
+    object1 = longestprefixsolution()
+    print(object1.prefixsolution([input("Enter strings : ") for i in range(4)]))
+        
+        
+        
